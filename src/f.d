38b9353aeb631d10b93d049b48cc4e07123b6c5d
Dmitry Ponyatov <dponyatov@gmail.com>
2023-12-04 02:01:14 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/f.d
+++ src/f.d
@@ -1,14 +1,10 @@
 import std.stdio;
-import std.file;
+
+import parser;
 
 void main(string[] args) {
     writeln(args);
     foreach (df; args[1 .. $]) {
         parse(df);
     }
-}
-
-void parse(string scriptfile) {
-    auto src = readText(scriptfile);
-    writeln(src);
 }
